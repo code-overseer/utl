6e77b34f4f4e38b8a9651e947b0d770d874724d5
Bryan Wong <wongjengyan@gmail.com>
2024-04-30 16:32:29 +0900

    disable non constant_eval check support

--- src/utl/public/utl/string/utl_constant_p.h
+++ src/utl/public/utl/string/utl_constant_p.h
@@ -7,13 +7,9 @@
 #if UTL_HAS_BUILTIN(__builtin_is_constant_evaluated)
 
 #  define UTL_CONSTANT_P(...) __builtin_is_constant_evaluated()
-#  define UTL_CONSTEXPR_BRANCHABLE constexpr
-#  define UTL_CONSTEXPR_BRANCHABLE_CXX14 UTL_CONSTEXPR_CXX14
 
 #elif defined(__builtin_constant_p)
 #  define UTL_CONSTANT_P(...) __builtin_constant_p(__VA_ARGS__)
-#  define UTL_CONSTEXPR_BRANCHABLE constexpr
-#  define UTL_CONSTEXPR_BRANCHABLE_CXX14 UTL_CONSTEXPR_CXX14
 
 #elif defined(UTL_COMPILER_MSVC)
 UTL_NAMESPACE_BEGIN
@@ -33,13 +29,9 @@ constexpr bool is_constant_evaluated() noexcept {
 UTL_NAMESPACE_END
 
 #  define UTL_CONSTANT_P(...) UTL_SCOPE details::is_constant_evaluated()
-#  define UTL_CONSTEXPR_BRANCHABLE constexpr
-#  define UTL_CONSTEXPR_BRANCHABLE_CXX14 UTL_CONSTEXPR_CXX14
 
 #else
 
-#  define UTL_CONSTANT_P(...) 0
-#  define UTL_CONSTEXPR_BRANCHABLE
-#  define UTL_CONSTEXPR_BRANCHABLE_CXX14
+#  error Unsupported compiler
 
 #endif
