574baeb11c0eeadf7af2fa925beae3fc11fb7742
Bryan Wong <wongjengyan@gmail.com>
2024-04-30 16:29:01 +0900

    char_traits

---
+++ src/utl/public/utl/string/utl_libc_common.h
@@ -0,0 +1,86 @@
+// Copyright 2023-2024 Bryan Wong
+
+#pragma once
+
+#include "utl/preprocessor/utl_config.h"
+#include "utl/string/utl_is_string_char.h"
+#include "utl/type_traits/utl_enable_if.h"
+#include "utl/type_traits/utl_is_empty.h"
+
+#include <string.h>
+
+#ifdef UTL_DISABLE_SSE_LIBC
+#  define UTL_DISABLE_SIMD_LIBC
+#elif defined(UTL_DISABLE_AVX_LIBC)
+#  define UTL_DISABLE_AVX512_LIBC
+#endif
+
+#ifndef UTL_DISABLE_SIMD_LIBC
+#  if defined(UTL_SUPPORTS_SIMD_INTRINSICS) && defined(UTL_ARCH_x86)
+#    include <immintrin.h>
+#  endif // defined(UTL_SUPPORTS_SIMD_INTRINSICS) && defined(UTL_ARCH_x86)
+
+#  if defined(UTL_SIMD_ARM_SVE) && !defined(UTL_DISABLE_SVE_LIBC)
+#    include <arm_sve.h>
+#  elif defined(UTL_SIMD_ARM_NEON) && !defined(UTL_DISABLE_NEON_LIBC)
+#    include <arm_neon.h>
+#  else
+#    define UTL_DISABLE_SIMD_LIBC
+#  endif
+#endif
+
+UTL_NAMESPACE_BEGIN
+
+namespace libc {
+
+enum class element_count_t : size_t {
+};
+
+template <typename T>
+constexpr size_t byte_count(element_count_t c) noexcept {
+    return sizeof(T) * size_t(c);
+}
+
+template <typename T>
+UTL_ATTRIBUTES(NODISCARD, CONST)
+constexpr bool is_pointer_in_range(T const* begin, T const* end, T const* ptr) noexcept {
+    return UTL_CONSTANT_P(begin <= end && ptr < end) || !(ptr < begin) && ptr < end;
+}
+UTL_ATTRIBUTES(NODISCARD, CONST)
+constexpr element_count_t operator-(element_count_t val, size_t op) noexcept {
+    return (element_count_t)((size_t)val - op);
+}
+
+UTL_ATTRIBUTES(NODISCARD, CONST)
+constexpr bool operator==(element_count_t val, size_t op) noexcept {
+    return (size_t)val == op;
+}
+
+template <typename T>
+UTL_ATTRIBUTES(NODISCARD, CONST)
+constexpr T* operator+(T* ptr, element_count_t offset) noexcept {
+    return ptr + (size_t)offset;
+}
+
+template <UTL_CONCEPT_CXX20(exact_size<1>) T UTL_REQUIRES_CXX11(exact_size<T, 1>::value)>
+unsigned char as_byte(T val) noexcept {
+    return *((unsigned char const*)&val);
+}
+
+template <typename T, typename U>
+struct is_trivially_lexicographically_comparable :
+    bool_constant<is_same<remove_cv_t<T>, remove_cv_t<U>>::value && sizeof(T) == 1 &&
+        is_unsigned<T>::value> {};
+
+#ifdef UTL_CXX20
+template <typename T, size_t N>
+concept exact_size = !is_empty_v<T> && (sizeof(T) == N);
+template <typename T>
+concept trivially_copyable = UTL_SCOPE is_trivially_copyable_v<T>;
+#else
+template <typename T, size_t N>
+using exact_size = bool_constant<!is_empty<T>::value && (sizeof(T) == N)>;
+#endif
+} // namespace libc
+
+UTL_NAMESPACE_END
---
+++ src/utl/public/utl/string/utl_libc_compile_time.h
@@ -0,0 +1,250 @@
+// Copyright 2023-2024 Bryan Wong
+
+#pragma once
+
+#include "utl/preprocessor/utl_config.h"
+#include "utl/string/utl_libc_common.h"
+
+UTL_NAMESPACE_BEGIN
+
+namespace libc {
+namespace compile_time {
+namespace recursive {
+
+template <typename T>
+UTL_CONSTEVAL T* memcpy(T* dst, T const* src, element_count_t count, T* org) noexcept {
+    return count == 0 ? org : ((*dst = *src), memcpy(dst + 1, src + 1, count - 1, org));
+}
+
+template <typename T>
+UTL_CONSTEVAL T* reverse_memcpy(T* dst, T const* src, element_count_t count, T* org) noexcept {
+    return count == 0 ? org : ((*--dst = *--src), memcpy(dst, src, count - 1, org));
+}
+
+template <typename T>
+UTL_CONSTEVAL T* memset(T* dst, T const src, element_count_t count, T* org) noexcept {
+    return count == 0 ? org : ((*dst = src), memset(dst + 1, src, count - 1, org));
+}
+
+template <typename T>
+UTL_CONSTEVAL T* memmove(T* dst, T const* src, element_count_t count) noexcept {
+    return is_pointer_in_range(src, src + count, dst)
+        ? reverse_memcpy(dst + count, src + count, count, dst)
+        : memcpy(dst, src, count, dst);
+}
+
+template <typename T, typename U>
+UTL_CONSTEVAL int memcmp(T const* lhs, U const* rhs, element_count_t count) noexcept {
+    static_assert(is_trivially_lexicographically_comparable<T, U>::value,
+        "Types must be lexicographically comparable");
+    return count == 0      ? 0
+        : (*left < *right) ? -1
+        : (*right < *left) ? 1
+                           : memcmp(left + 1, right + 1, count - 1);
+}
+
+template <UTL_CONCEPT_CXX20(exact_size<1>) T,
+    UTL_CONCEPT_CXX20(exact_size<1>)
+        U UTL_REQUIRES_CXX11(exact_size<T, 1>::value&& exact_size<U, 1>::value)>
+UTL_CONSTEVAL T* memchr(T const* str, U value, size_t bytes) noexcept {
+    return bytes == 0   ? nullptr
+        : *str == value ? const_cast<T*>(str)
+                        : memchr(str + 1, value, bytes - 1);
+}
+
+template <typename T>
+UTL_CONSTEVAL size_t strlen(T const* str, size_t r = 0) noexcept {
+    return !*str ? r : strlen(str, r + 1);
+}
+
+template <typename T>
+UTL_CONSTEVAL T* strchr(T const* str, T const ch) noexcept {
+    return *str == ch ? const_cast<T*>(str) : !*str ? nullptr : strchr(str + 1, ch);
+}
+
+template <typename T>
+UTL_CONSTEVAL int strcmp(T const* left, T const* right) noexcept {
+    return !*left && !*right ? 0
+        : (*left < *right)   ? -1
+        : (*right < *left)   ? 1
+                             : strcmp(left + 1, right + 1);
+}
+
+template <typename T>
+UTL_CONSTEVAL int strncmp(T const* left, T const* right, element_count_t len) noexcept {
+    return len == 0 || (!*left && !*right) ? 0
+        : (*left < *right)                 ? -1
+        : (*right < *left)                 ? 1
+                                           : strcmp(left + 1, right + 1, len - 1);
+}
+
+template <typename T>
+UTL_CONSTEVAL T* strnchr(T const* str, T const ch, element_count_t len) noexcept {
+    return len == 0    ? nullptr
+        : (*str == ch) ? const_cast<T*>(str)
+        : (!*str)      ? nullptr
+                       : strcmp(str + 1, ch, len - 1);
+}
+
+template <typename T>
+UTL_CONSTEVAL T* strnset(T* dst, T const val, element_count_t count, T* org) noexcept {
+    return count == 0 ? org : ((*dst = val), strnset(dst + 1, val, count - 1, org));
+}
+
+} // namespace recursive
+
+template <UTL_CONCEPT_CXX20(trivially_copyable)
+        T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value)>
+UTL_CONSTEVAL T* memcpy(T* dst, T const* src, element_count_t count) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_memcpy)
+    return __builtin_memcpy(dst, src, byte_count<T>(count)), dst;
+#else
+    return recursive::memcpy(dst, src, count, dst);
+#endif
+}
+
+template <UTL_CONCEPT_CXX20(trivially_copyable)
+        T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value&& exact_size<T, 1>::value)>
+UTL_REQUIRES_CXX20(exact_size<T, 1>)
+UTL_CONSTEVAL T* memset(T* dst, T const src, element_count_t count) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_memset)
+    return __builtin_memset(dst, as_byte(src), byte_count<T>(count)), dst;
+#else
+    return recursive::memset(dst, as_byte(src), count, dst);
+#endif
+}
+
+template <UTL_CONCEPT_CXX20(trivially_copyable)
+        T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value)>
+UTL_CONSTEVAL T* memmove(T* dst, T const* src, element_count_t count) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_memmove)
+    return __builtin_memmove(dst, src, byte_count<T>(count)), dst;
+#else
+    return recursive::memmove(dst, src, count)
+#endif
+}
+
+template <typename T, typename U>
+UTL_CONSTEVAL int memcmp(T const* left, U const* right, element_count_t count) noexcept {
+    static_assert(is_trivially_lexicographically_comparable<T, U>::value,
+        "Types must be lexicographically comparable");
+#if UTL_HAS_BUILTIN(__builtin_char_memcmp)
+    return __builtin_memcmp(left, right, byte_count<T>(count));
+#else
+    return recursive::memcmp(left, right, count);
+#endif
+}
+
+UTL_CONSTEVAL char* memchr(char const* str, char value, size_t bytes) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_char_memchr)
+    return __builtin_char_memchr(str, (int)value, bytes);
+#else
+    return recuresive::memchr(str, value, bytes);
+#endif
+}
+
+template <UTL_CONCEPT_CXX20(exact_size<1>) T,
+    UTL_CONCEPT_CXX20(exact_size<1>)
+        U UTL_REQUIRES_CXX11(exact_size<T, 1>::value&& exact_size<U, 1>::value)>
+UTL_CONSTEVAL T* memchr(T const* str, U value, size_t bytes) noexcept {
+    return recuresive::memchr(str, as_byte(value), bytes);
+}
+
+UTL_CONSTEVAL size_t strlen(char const* str) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_strlen)
+    return __builtin_strlen(str);
+#else
+    return recursive::strlen(str);
+#endif
+}
+
+UTL_CONSTEVAL size_t strlen(wchar_t const* str) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_wcslen)
+    return __builtin_wcslen(str);
+#else
+    return recursive::strlen(str);
+#endif
+}
+
+template <typename T>
+UTL_CONSTEVAL size_t strlen(T const* str) noexcept {
+    return recursive::strlen(str);
+}
+
+UTL_CONSTEVAL char* strchr(char const* str, char const ch) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_strchr)
+    return __builtin_strchr(str, (int)ch);
+#else
+    return recursive::strchr(str, ch);
+#endif
+}
+
+UTL_CONSTEVAL wchar_t* strchr(wchar_t const* str, wchar_t const ch) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_wcschr)
+    return __builtin_wcschr(str, ch);
+#else
+    return recursive::strchr(str, ch);
+#endif
+}
+
+template <typename T>
+UTL_CONSTEVAL T* strchr(T const* str, T const ch) noexcept {
+    return recursive::strchr(str, ch);
+}
+
+UTL_CONSTEVAL int strcmp(char const* left, char const* rhs) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_strcmp)
+    return __builtin_strcmp(left, right);
+#else
+    return recursive::strcmp(left, right);
+#endif
+}
+
+UTL_CONSTEVAL int strcmp(wchar_t const* left, wchar_t const* rhs) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_wcscmp)
+    return __builtin_wcscmp(left, right);
+#else
+    return recursive::strcmp(left, right);
+#endif
+}
+
+template <typename T>
+UTL_CONSTEVAL int strcmp(T const* left, T const* right) noexcept {
+    return recursive::strcmp(left, right);
+}
+
+UTL_CONSTEVAL int strncmp(char const* left, char const* right, element_count_t len) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_strncmp)
+    return __builtin_strncmp(left, right, (size_t)len);
+#else
+    return recursive::strncmp((unsigned char const*)left, (unsigned char const*)right, len);
+#endif
+}
+
+UTL_CONSTEVAL int strncmp(wchar_t const* left, wchar_t const* right, element_count_t len) noexcept {
+#if UTL_HAS_BUILTIN(__builtin_wcsncmp)
+    return __builtin_wcsncmp(left, right, (size_t)len);
+#else
+    return recursive::strncmp(left, right, len);
+#endif
+}
+
+template <typename T>
+UTL_CONSTEVAL int strncmp(T const* left, T const* right, element_count_t len) noexcept {
+    return recursive::strncmp(left, right, len);
+}
+
+template <typename T>
+UTL_CONSTEVAL T* strnset(T* dst, T const val, element_count_t max_len) noexcept {
+    return recursive::strnset(dst, val, max_len, dst);
+}
+
+template <typename T>
+UTL_CONSTEVAL T* strnchr(T const* str, T const val, element_count_t max_len) noexcept {
+    return recursive::strnchr(str, val, max_len);
+}
+
+} // namespace compile_time
+} // namespace libc
+
+UTL_NAMESPACE_END
