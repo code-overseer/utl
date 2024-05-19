f7284c4c7f895f42f59c3956b0c2ba6e12284a9a
Bryan Wong <wongjengyan@gmail.com>
2024-05-19 23:51:25 +0900

    better collapse

--- src/utl/public/utl/memory/utl_start_lifetime.h
+++ src/utl/public/utl/memory/utl_start_lifetime.h
@@ -13,65 +13,85 @@ UTL_NAMESPACE_BEGIN
 
 #define UTL_START_LIFETIME_ATTRIBUTES UTL_ATTRIBUTES(NODISCARD, ALWAYS_INLINE)
 
-template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
-UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T*, UTL_TRAIT_is_implicit_lifetime(T))
-    start_lifetime_as(void* p) noexcept {
+namespace details {
+namespace lifetime {
+template <typename T>
+UTL_START_LIFETIME_ATTRIBUTES T* start(void* p) noexcept {
     using byte_type = unsigned char;
     auto bytes = ::new (p) byte_type[sizeof(T)];
     auto started = reinterpret_cast<T*>(bytes);
-    (void)*started;
+    (void)*started; // collapse the wave-function
     return started;
 }
 
-template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
-UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T const*, UTL_TRAIT_is_implicit_lifetime(T))
-    start_lifetime_as(void const* p) noexcept {
-    return start_lifetime_as(const_cast<void*>(p));
-}
-
-template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
-UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T volatile*, UTL_TRAIT_is_implicit_lifetime(T))
-    start_lifetime_as(void volatile* p) noexcept {
-    return start_lifetime_as(const_cast<void*>(p));
-}
-
-template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
-UTL_ATTRIBUTES(ALWAYS_INLINE)
-UTL_ENABLE_IF_CXX11(T const volatile*, UTL_TRAIT_is_implicit_lifetime(T))
-    start_lifetime_as(void const volatile* p) noexcept {
-    return start_lifetime_as(const_cast<void*>(p));
-}
-
-template <UTL_CONCEPT_CXX20(complete) T>
-UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T*, UTL_TRAIT_is_complete(T))
-    start_lifetime_as_array(void* p, size_t n) noexcept {
+template <typename T>
+UTL_START_LIFETIME_ATTRIBUTES T* start_array(void* p, size_t n) noexcept {
     if (!n) {
         return reinterpret_cast<T*>(p);
     }
 
     using byte_type = unsigned char;
     auto bytes = ::new (p) byte_type[sizeof(T) * n];
+    if (n == 1) {
+        auto started = reinterpret_cast<T(*)[1]>(bytes);
+        return *started; // collapse wave-function by treating as array
+    }
+
     auto started = reinterpret_cast<T*>(bytes);
-    (void)*started;
+    // pointer arithmetic is only defined if pointer is a pointer to an array element
+    (void)(started + n); // collapse wave-function by treating as array
     return started;
+}
+} // namespace lifetime
+} // namespace details
+
+template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
+UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T*, UTL_TRAIT_is_implicit_lifetime(T))
+    start_lifetime_as(void* p) noexcept {
+    return details::lifetime::start<T>(p);
+}
+
+template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
+UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T const*, UTL_TRAIT_is_implicit_lifetime(T))
+    start_lifetime_as(void const* p) noexcept {
+    return details::lifetime::start<T const>(const_cast<void*>(p));
+}
+
+template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
+UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T volatile*, UTL_TRAIT_is_implicit_lifetime(T))
+    start_lifetime_as(void volatile* p) noexcept {
+    return details::lifetime::start<T volatile>(const_cast<void*>(p));
+}
+
+template <UTL_CONCEPT_CXX20(implicit_lifetime) T>
+UTL_ATTRIBUTES(ALWAYS_INLINE)
+UTL_ENABLE_IF_CXX11(T const volatile*, UTL_TRAIT_is_implicit_lifetime(T))
+    start_lifetime_as(void const volatile* p) noexcept {
+    return details::lifetime::start<T const volatile>(const_cast<void*>(p));
+}
+
+template <UTL_CONCEPT_CXX20(complete) T>
+UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T*, UTL_TRAIT_is_complete(T))
+    start_lifetime_as_array(void* p, size_t n) noexcept {
+    return details::lifetime::start_array<T>(const_cast<void*>(p), n);
 }
 
 template <UTL_CONCEPT_CXX20(complete) T>
 UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T const*, UTL_TRAIT_is_complete(T))
     start_lifetime_as_array(void const* p, size_t n) noexcept {
-    return start_lifetime_as_array(const_cast<void*>(p));
+    return details::lifetime::start_array<T const>(const_cast<void*>(p), n);
 }
 
 template <UTL_CONCEPT_CXX20(complete) T>
 UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T volatile*, UTL_TRAIT_is_complete(T))
     start_lifetime_as_array(void volatile* p, size_t n) noexcept {
-    return start_lifetime_as_array(const_cast<void*>(p));
+    return details::lifetime::start_array<T volatile>(const_cast<void*>(p), n);
 }
 
 template <UTL_CONCEPT_CXX20(complete) T>
 UTL_START_LIFETIME_ATTRIBUTES UTL_ENABLE_IF_CXX11(T const volatile*, UTL_TRAIT_is_complete(T))
     start_lifetime_as_array(void const volatile* p, size_t n) noexcept {
-    return start_lifetime_as_array(const_cast<void*>(p));
+    return details::lifetime::start_array<T const volatile>(const_cast<void*>(p), n);
 }
 
 #undef UTL_START_LIFETIME_ATTRIBUTES
