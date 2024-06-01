e5c7cf974135dd1249a408d2f644d41aff5d3b26
Bryan Wong <wongjengyan@gmail.com>
2024-06-01 22:08:11 +0900

    Fix allocator concept

--- src/utl/public/utl/concepts/utl_allocator_type.h
+++ src/utl/public/utl/concepts/utl_allocator_type.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "utl/concepts/utl_assignable_from.h"
 #include "utl/concepts/utl_copy_constructible.h"
 #include "utl/concepts/utl_empty_type.h"
 #include "utl/concepts/utl_equality_comparable.h"
@@ -18,22 +19,51 @@ namespace details {
 namespace allocator {
 
 template <typename T>
-concept comparable = empty_type(T) requires {
+concept comparability = empty_type<T> || requires {
     typename T::is_always_equal;
     requires T::is_always_equal::value;
 } || (UTL_SCOPE equality_comparable<T> && requires(T const& l, T const& r) {
     { l == r } noexcept;
     { l != r } noexcept;
 });
-}
+
+template <typename T>
+concept copy_propogation = requires { typename T::propagate_on_container_copy_assignment; } &&
+    T::propagate_on_container_copy_assignment::value;
+
+template <typename T>
+concept move_propogation = requires { typename T::propagate_on_container_move_assignment; } &&
+    T::propagate_on_container_move_assignment::value;
+
+template <typename T>
+concept swap_propogation =
+    requires { typename T::propagate_on_container_swap; } && T::propagate_on_container_swap::value;
+
+template <typename T>
+concept copy_assignability =
+    !copy_propogation<T> || (assignable_from<T&, T const&> && requires(T& l, T const& r) {
+        { l = r } noexcept;
+    });
+
+template <typename T>
+concept move_assignability =
+    !move_propogation<T> || (assignable_from<T&, T&&> && requires(T& l, T&& r) {
+        { l = UTL_SCOPE move(r) } noexcept;
+    });
+
+template <typename T>
+concept swappability = !swap_propogation<T> || (swappable<T> && is_nothrow_swappable_v<T>);
+
+} // namespace allocator
 } // namespace details
 
 template <typename T>
-concept allocator_type =
-    copy_constructible<T> && move_constructible<T> && swappable<T> && requires(T t) {
+concept allocator_type = copy_constructible<T> && move_constructible<T> &&
+    requires(T t) {
         { T(t) } noexcept;
         { T(UTL_SCOPE move(t)) } noexcept;
-    } && is_nothrow_swappable_v<T> && details::allocator::comparable<T>;
+    } && details::allocator::comparability<T> && details::allocator::copy_assignability<T> &&
+    details::allocator::move_assignability<T> && details::allocator::swappability<T>;
 
 UTL_NAMESPACE_END
 #endif
