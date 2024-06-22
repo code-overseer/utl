5a73a2bc44bf4039aa29d6420d7f6d9d3d60c003
Bryan Wong <wongjengyan@gmail.com>
2024-06-22 21:33:13 +0900

    implement sentinel_for

---
+++ src/utl/public/utl/iterator/utl_sentinel_for.h
@@ -0,0 +1,48 @@
+// Copyright 2023-2024 Bryan Wong
+
+#pragma once
+
+#include "utl/preprocessor/utl_config.h"
+#include "utl/type_traits/utl_constants.h"
+
+#if UTL_CXX20
+#  include "utl/concepts/utl_equality_comparable.h"
+#  include "utl/concepts/utl_semiregular.h"
+#  include "utl/iterator/utl_input_or_output_iterator.h"
+UTL_NAMESPACE_BEGIN
+template <typename S, typename I>
+concept sentinel_for = UTL_SCOPE semiregular<T> && UTL_SCOPE input_or_output_iterator<I> &&
+    UTL_SCOPE details::equality_comparable::weak<S, I>;
+
+template <typename S, typename I>
+struct is_sentinel_for : UTL_SCOPE bool_constant<sentinel_for<S, I>> {};
+
+template <typename S, typename I>
+inline constexpr bool is_sentinel_for_v = sentinel_for<S, I>;
+
+UTL_NAMESPACE_END
+
+#else // UTL_CXX20
+
+#  include "utl/type_traits/utl_is_copyable.h"
+#  include "utl/type_traits/utl_is_default_constructible.h"
+#  include "utl/type_traits/utl_is_equality_comparable.h"
+#  include "utl/type_traits/utl_is_inequality_comparable.h"
+#  include "utl/type_traits/utl_logical_traits.h"
+
+UTL_NAMESPACE_BEGIN
+
+template <typename S, typename I>
+struct is_sentinel_for :
+    UTL_SCOPE conjunction<UTL_SCOPE is_copyable<T>, UTL_SCOPE is_default_constructible<T>,
+        UTL_SCOPE is_input_or_output_iterator<I>, UTL_SCOPE is_equality_comparable<S, I>,
+        UTL_SCOPE is_inequality_comparable<S, I>, UTL_SCOPE is_equality_comparable<I, S>,
+        UTL_SCOPE is_inequality_comparable<I, S>> {};
+
+#  if UTL_CXX14
+template <typename S, typename I>
+UTL_INLINE_CXX17 constexpr bool is_sentinel_for_v = is_sentinel_for<S, I>::value;
+#  endif
+
+UTL_NAMESPACE_END
+#endif // UTL_CXX20
