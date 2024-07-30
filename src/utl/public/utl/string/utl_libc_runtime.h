d156c558c9cb8bf18defb064444b1c63280e9d7c
Bryan Wong <wongjengyan@gmail.com>
2024-07-30 23:13:30 +0900

    libc string

--- src/utl/public/utl/string/utl_libc_runtime.h
+++ src/utl/public/utl/string/utl_libc_runtime.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "utl/preprocessor/utl_config.h"
+
 #include "utl/string/utl_libc_common.h"
 #include "utl/utility/utl_signs.h"
 
@@ -15,10 +16,9 @@ namespace libc {
 namespace runtime {
 namespace standard {
 
-template <UTL_CONCEPT_CXX20(trivially_copyable)
-        T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value)>
-UTL_ATTRIBUTE(ALWAYS_INLINE)
-T* memcpy(T* UTL_RESTRICT dst, T const* UTL_RESTRICT src, element_count_t count) noexcept {
+template <UTL_CONCEPT_CXX20(trivially_copyable) T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value)>
+UTL_ATTRIBUTE(ALWAYS_INLINE) T* memcpy(
+    T* UTL_RESTRICT dst, T const* UTL_RESTRICT src, element_count_t count) noexcept {
 #if UTL_HAS_BUILTIN(__builtin_memcpy)
     return (T*)__builtin_memcpy(dst, src, byte_count<T>(count));
 #else
@@ -26,10 +26,8 @@ T* memcpy(T* UTL_RESTRICT dst, T const* UTL_RESTRICT src, element_count_t count)
 #endif
 }
 
-template <UTL_CONCEPT_CXX20(trivially_copyable)
-        T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value)>
-UTL_ATTRIBUTE(ALWAYS_INLINE)
-T* memmove(T* dst, T const* src, element_count_t count) noexcept {
+template <UTL_CONCEPT_CXX20(trivially_copyable) T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value)>
+UTL_ATTRIBUTE(ALWAYS_INLINE) T* memmove(T* dst, T const* src, element_count_t count) noexcept {
 #if UTL_HAS_BUILTIN(__builtin_memcpy)
     return (T*)__builtin_memmove(dst, src, byte_count<T>(count));
 #else
@@ -37,8 +35,8 @@ T* memmove(T* dst, T const* src, element_count_t count) noexcept {
 #endif
 }
 
-template <UTL_CONCEPT_CXX20(trivially_copyable)
-        T UTL_REQUIRES_CXX11(is_trivially_copyable<T>::value&& exact_size<T, 1>::value)>
+template <UTL_CONCEPT_CXX20(trivially_copyable) T UTL_REQUIRES_CXX11(
+    is_trivially_copyable<T>::value && exact_size<T, 1>::value)>
 UTL_REQUIRES_CXX20(exact_size<T, 1>)
 UTL_ATTRIBUTE(ALWAYS_INLINE) T* memset(T* dst, T const value, element_count_t count) noexcept {
 #if UTL_HAS_BUILTIN(__builtin_memcpy)
@@ -48,9 +46,8 @@ UTL_ATTRIBUTE(ALWAYS_INLINE) T* memset(T* dst, T const value, element_count_t co
 #endif
 }
 
-template <UTL_CONCEPT_CXX20(exact_size<1>) T,
-    UTL_CONCEPT_CXX20(exact_size<1>)
-        U UTL_REQUIRES_CXX11(exact_size<T, 1>::value&& exact_size<U, 1>::value)>
+template <UTL_CONCEPT_CXX20(exact_size<1>) T, UTL_CONCEPT_CXX20(exact_size<1>) U UTL_REQUIRES_CXX11(
+    exact_size<T, 1>::value && exact_size<U, 1>::value)>
 UTL_LIBC_INLINE_PURE T* memchr(T const* ptr, U value, size_t bytes) noexcept {
 #if UTL_HAS_BUILTIN(__builtin_char_memchr)
     return (T*)__builtin_char_memchr(ptr, as_byte(value), bytes);
