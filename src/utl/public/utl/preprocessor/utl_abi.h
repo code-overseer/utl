f1520f07ff0a3d4114ccf7fe7c1e38808c371ad9
Bryan Wong <wongjengyan@gmail.com>
2024-08-06 23:09:56 +0900

    add preprocessor configuration for abi visibility

--- src/utl/public/utl/preprocessor/utl_abi.h
+++ src/utl/public/utl/preprocessor/utl_abi.h
@@ -1,3 +1,121 @@
 /* Copyright 2023-2024 Bryan Wong */
 
 #pragma once
+
+#include "utl/preprocessor/utl_attributes.h"
+#include "utl/preprocessor/utl_compiler.h"
+#include "utl/preprocessor/utl_concatenation.h"
+#include "utl/preprocessor/utl_exceptions.h"
+#include "utl/preprocessor/utl_standard.h"
+#include "utl/preprocessor/utl_target.h"
+#include "utl/preprocessor/utl_to_string.h"
+
+#if UTL_COMPILER_MSVC
+
+#  if !UTL_HAS_DECLSPEC(dllexport) || !UTL_HAS_DECLSPEC(dllimport)
+#    error "Unrecognized compiler"
+#  endif
+
+#  ifdef UTL_BUILDING_LIBRARY
+#    define UTL_ABI_PUBLIC __declspec(dllexport)
+#    define __UTL_ATTRIBUTE_ABI_PUBLIC (DLLEXPORT)
+#    define __UTL_ATTRIBUTE_TYPE_AGGREGATE_ABI_PUBLIC
+#  else
+#    define UTL_ABI_PUBLIC __declspec(dllimport)
+#    define __UTL_ATTRIBUTE_ABI_PUBLIC (DLLIMPORT)
+#    define __UTL_ATTRIBUTE_TYPE_AGGREGATE_ABI_PUBLIC
+#  endif
+#  define UTL_ABI_PRIVATE
+#  define __UTL_ATTRIBUTE_ABI_PRIVATE
+#  ifdef __UTL_ATTRIBUTE_TYPE_AGGREGATE_ABI_PRIVATE
+#    error '__UTL_ATTRIBUTE_TYPE_AGGREGATE_ABI_PRIVATE' cannot be defined
+#  endif
+#else
+
+#  if !UTL_HAS_GNU_ATTRIBUTE(__visibility__) || !UTL_HAS_GNU_ATTRIBUTE(__visibility__)
+#    error "Unrecognized compiler"
+#  endif
+
+#  define UTL_ABI_PUBLIC __attribute__((__visibility__("default")))
+#  define UTL_ABI_PRIVATE __attribute__((__visibility__("hidden")))
+#  define __UTL_ATTRIBUTE_ABI_PUBLIC (VISIBILITY("default"))
+#  define __UTL_ATTRIBUTE_TYPE_AGGREGATE_ABI_PUBLIC
+#  define __UTL_ATTRIBUTE_ABI_PRIVATE (VISIBILITY("hidden"))
+#  define __UTL_ATTRIBUTE_TYPE_AGGREGATE_ABI_PRIVATE
+
+#endif
+
+#if !UTL_HAS_ATTRIBUTE(TYPE_VISIBILITY)
+/* For GNU compilers that don't have type visibility we must keep the templates visible */
+#  define UTL_PUBLIC_TEMPLATE __attribute__((__visibility__("default")))
+#  define __UTL_ATTRIBUTE_PUBLIC_TEMPLATE (VISIBILITY("default"))
+#  define __UTL_ATTRIBUTE_TYPE_AGGREGATE_PUBLIC_TEMPLATE
+#endif
+
+#define __UTL_ODR_SIGNATURE_1(_0, _1, _2, _3, _4) _0##_1##_2##_3##_4
+#define __UTL_ODR_SIGNATURE_0(_0, _1, _2, _3, _4) __UTL_ODR_SIGNATURE_1(_0, _1, _2, _3, _4)
+#define __UTL_ODR_SIGNATURE              \
+    UTL_TO_STRING(__UTL_ODR_SIGNATURE_0( \
+        CXX, UTL_CXX, __UTL_HARDENING_MODE, __UTL_ABI_EXCEPTION_TAG, UTL_COMPILER_TAG))
+
+#if UTL_HAS_GNU_ATTRIBUTE(__exclude_from_explicit_instantiation__)
+#  define __UTL_EXCLUDE_FROM_EXPLICIT_INSTANTIATION \
+      __attribute__((__exclude_from_explicit_instantiation__))
+#else
+#  define __UTL_EXCLUDE_FROM_EXPLICIT_INSTANTIATION
+#endif
+
+#if UTL_HAS_GNU_ATTRIBUTE(__abi_tag__)
+#  define __UTL_ABI_TAG(TAG) __attribute__((__abi_tag__(TAG)))
+#else
+#  define __UTL_ABI_TAG(TAG)
+#endif
+
+#define UTL_HIDE_FROM_ABI \
+    UTL_ABI_PRIVATE __UTL_EXCLUDE_FROM_EXPLICIT_INSTANTIATION __UTL_ABI_TAG(__UTL_ODR_SIGNATURE)
+#define __UTL_ATTRIBUTE_HIDE_FROM_ABI \
+    __UTL_ATTRIBUTE_ABI_PRIVATE(EXCLUDE_FROM_EXPLICIT_INSTANTIATION)(ABI_TAG(__UTL_ODR_SIGNATURE))
+#define __UTL_ATTRIBUTE_TYPE_AGGREGATE_HIDE_FROM_ABI
+
+/* virtual functions must be linked to the same symbol */
+#define UTL_HIDE_FROM_ABI_VIRTUAL UTL_ABI_PRIVATE __UTL_EXCLUDE_FROM_EXPLICIT_INSTANTIATION
+#define __UTL_ATTRIBUTE_HIDE_FROM_ABI_VIRTUAL \
+    __UTL_ATTRIBUTE_ABI_PRIVATE(EXCLUDE_FROM_EXPLICIT_INSTANTIATION)
+#define __UTL_ATTRIBUTE_TYPE_AGGREGATE_HIDE_FROM_ABI_VIRTUAL
+
+#if UTL_CXX17
+#  define UTL_ABI_PUBLIC_VARIABLE inline
+#else
+#  define UTL_ABI_PUBLIC_VARIABLE UTL_ABI_PUBLIC
+#endif
+
+#if UTL_WITH_EXCEPTIONS
+#  define __UTL_ABI_EXCEPTION_TAG e
+#else
+#  define __UTL_ABI_EXCEPTION_TAG n
+#endif
+
+#ifndef UTL_COMPILER_TAG
+#  error Undefined compiler tag
+#endif
+
+#ifndef __UTL_ABI_EXCEPTION_TAG
+#  error Undefined exception tag
+#endif
+
+/* TODO: Use debug/opt level */
+#define __UTL_HARDENING_MODE n
+
+#define UTL_EXTERN_C extern "C"
+#define UTL_EXTERN_C_BEGIN UTL_EXTERN_C {
+#define UTL_EXTERN_C_END }
+
+#if UTL_COMPILER_MSVC && UTL_CXX20
+#  define UTL_EXTERN_CXX extern "C++"
+#  define UTL_EXTERN_CXX_BEGIN UTL_EXTERN_CXX {
+#  define UTL_EXTERN_CXX_END }
+#else
+#  define UTL_EXTERN_CXX
+#  define UTL_EXTERN_CXX_BEGIN
+#  define UTL_EXTERN_CXX_END
+#endif
--- src/utl/public/utl/preprocessor/utl_attributes.h
+++ src/utl/public/utl/preprocessor/utl_attributes.h
@@ -33,15 +33,32 @@
 #  define UTL_NODISCARD
 #endif /* UTL_HAS_CPP_ATTRIBUTE(nodiscard) */
 
+#if UTL_HAS_CPP_ATTRIBUTE(noreturn)
+#  define UTL_NORETURN [[noreturn]]
+#  define __UTL_ATTRIBUTE_NORETURN noreturn
+#  define __UTL_ATTRIBUTE_TYPE_CPP_NORETURN
+#else
+#  define UTL_NORETURN
+#endif
+
 #if UTL_HAS_CPP_ATTRIBUTE(msvc::lifetimebound)
+#  define UTL_LIFETIMEBOUND [[msvc::lifetimebound]]
 #  define __UTL_ATTRIBUTE_LIFETIMEBOUND msvc::lifetimebound
 #  define __UTL_ATTRIBUTE_TYPE_CPP_LIFETIMEBOUND
 #elif UTL_HAS_CPP_ATTRIBUTE(clang::lifetimebound) /* UTL_HAS_CPP_ATTRIBUTE(msvc::lifetimebound) */
+#  define UTL_LIFETIMEBOUND [[clang::lifetimebound]]
 #  define __UTL_ATTRIBUTE_LIFETIMEBOUND clang::lifetimebound
 #  define __UTL_ATTRIBUTE_TYPE_CPP_LIFETIMEBOUND
+#elif UTL_HAS_CPP_ATTRIBUTE(gnu::lifetimebound) /* UTL_HAS_CPP_ATTRIBUTE(msvc::lifetimebound) */
+#  define UTL_LIFETIMEBOUND [[gnu::lifetimebound]]
+#  define __UTL_ATTRIBUTE_LIFETIMEBOUND gnu::lifetimebound
+#  define __UTL_ATTRIBUTE_TYPE_CPP_LIFETIMEBOUND
 #elif UTL_HAS_GNU_ATTRIBUTE(__lifetimebound__) /* UTL_HAS_CPP_ATTRIBUTE(msvc::lifetimebound) */
+#  define UTL_LIFETIMEBOUND __attribute__((__lifetimebound__))
 #  define __UTL_ATTRIBUTE_LIFETIMEBOUND __lifetimebound__
 #  define __UTL_ATTRIBUTE_TYPE_GNU_LIFETIMEBOUND
+#else
+#  define UTL_LIFETIMEBOUND
 #endif /* UTL_HAS_CPP_ATTRIBUTE(msvc::lifetimebound) */
 
 #if UTL_HAS_CPP_ATTRIBUTE(gnu::pure)
@@ -104,6 +121,23 @@
 #  define __UTL_ATTRIBUTE_TYPE_MSVC_EXT_ALWAYS_INLINE
 #endif /* UTL_HAS_CPP_ATTRIBUTE(clang::always_inline) */
 
+#if UTL_HAS_CPP_ATTRIBUTE(msvc::noinline)
+#  define __UTL_ATTRIBUTE_NOINLINE msvc::noinline
+#  define __UTL_ATTRIBUTE_TYPE_CPP_NOINLINE
+#elif UTL_HAS_CPP_ATTRIBUTE(clang::noinline)
+#  define __UTL_ATTRIBUTE_NOINLINE clang::noinline
+#  define __UTL_ATTRIBUTE_TYPE_CPP_NOINLINE
+#elif UTL_HAS_CPP_ATTRIBUTE(gnu::noinline)
+#  define __UTL_ATTRIBUTE_NOINLINE gnu::noinline
+#  define __UTL_ATTRIBUTE_TYPE_CPP_NOINLINE
+#elif UTL_HAS_GNU_ATTRIBUTE(__noinline__)
+#  define __UTL_ATTRIBUTE_NOINLINE __noinline__
+#  define __UTL_ATTRIBUTE_TYPE_GNU_NOINLINE
+#elif UTL_HAS_DECLSPEC(noinline)
+#  define __UTL_ATTRIBUTE_NOINLINE noinline
+#  define __UTL_ATTRIBUTE_TYPE_DECLSPEC_NOINLINE
+#endif /* UTL_HAS_CPP_ATTRIBUTE(clang::noinline) */
+
 #if UTL_HAS_CPP_ATTRIBUTE(clang::malloc)
 #  define __UTL_ATTRIBUTE_MALLOC clang::malloc
 #  define __UTL_ATTRIBUTE_TYPE_CPP_MALLOC
@@ -119,6 +153,9 @@
 #    define __UTL_ATTRIBUTE_TYPE_DECLSPEC_MALLOC
 #  elif UTL_HAS_DECLSPEC(allocator)
 #    define __UTL_ATTRIBUTE_MALLOC allocator
+#    define __UTL_ATTRIBUTE_TYPE_DECLSPEC_MALLOC
+#  elif UTL_HAS_DECLSPEC(restrict)
+#    define __UTL_ATTRIBUTE_MALLOC restrict
 #    define __UTL_ATTRIBUTE_TYPE_DECLSPEC_MALLOC
 #  endif
 #endif /* UTL_HAS_CPP_ATTRIBUTE(clang::malloc) */
@@ -180,6 +217,31 @@
 #  define __UTL_ATTRIBUTE_TYPE_DECLSPEC_DLLIMPORT
 #endif
 
+#if UTL_HAS_CPP_ATTRIBUTE(clang::exclude_from_explicit_instantiation)
+#  define __UTL_ATTRIBUTE_EXCLUDE_FROM_EXPLICIT_INSTANTIATION \
+      clang::exclude_from_explicit_instantiation
+#  define __UTL_ATTRIBUTE_TYPE_CPP_EXCLUDE_FROM_EXPLICIT_INSTANTIATION
+#elif UTL_HAS_CPP_ATTRIBUTE(gnu::exclude_from_explicit_instantiation)
+#  define __UTL_ATTRIBUTE_EXCLUDE_FROM_EXPLICIT_INSTANTIATION \
+      gnu::exclude_from_explicit_instantiation
+#  define __UTL_ATTRIBUTE_TYPE_CPP_EXCLUDE_FROM_EXPLICIT_INSTANTIATION
+#elif UTL_HAS_GNU_ATTRIBUTE(__exclude_from_explicit_instantiation__)
+#  define __UTL_ATTRIBUTE_EXCLUDE_FROM_EXPLICIT_INSTANTIATION \
+      __exclude_from_explicit_instantiation__
+#  define __UTL_ATTRIBUTE_TYPE_GNU_EXCLUDE_FROM_EXPLICIT_INSTANTIATION
+#endif
+
+#if UTL_HAS_CPP_ATTRIBUTE(clang::abi_tag)
+#  define __UTL_ATTRIBUTE_ABI_TAG clang::abi_tag
+#  define __UTL_ATTRIBUTE_TYPE_CPP_ABI_TAG(STR)
+#elif UTL_HAS_CPP_ATTRIBUTE(gnu::abi_tag)
+#  define __UTL_ATTRIBUTE_ABI_TAG gnu::abi_tag
+#  define __UTL_ATTRIBUTE_TYPE_CPP_ABI_TAG(STR)
+#elif UTL_HAS_GNU_ATTRIBUTE(__abi_tag__)
+#  define __UTL_ATTRIBUTE_ABI_TAG __abi_tag__
+#  define __UTL_ATTRIBUTE_TYPE_GNU_ABI_TAG(STR)
+#endif
+
 #if UTL_HAS_CPP_ATTRIBUTE(clang::reinitializes)
 #  define __UTL_ATTRIBUTE_REINITIALIZES clang::reinitializes
 #  define __UTL_ATTRIBUTE_TYPE_CPP_REINITIALIZES
@@ -203,17 +265,19 @@
 #endif /* UTL_HAS_CPP_ATTRIBUTE(maybe_unused) */
 
 #if UTL_HAS_CPP_ATTRIBUTE(fallthrough)
+#  define UTL_FALLTHROUGH [[fallthrough]]
 #  define __UTL_ATTRIBUTE_FALLTHROUGH fallthrough
 #  define __UTL_ATTRIBUTE_TYPE_CPP_FALLTHROUGH
 #elif UTL_HAS_CPP_ATTRIBUTE(gnu::fallthrough)
-#  define __UTL_ATTRIBUTE_FALLTHROUGH gnu::fallthrough
-#  define __UTL_ATTRIBUTE_TYPE_CPP_FALLTHROUGH
-#elif UTL_HAS_CPP_ATTRIBUTE(gnu::fallthrough)
+#  define UTL_FALLTHROUGH [[gnu::fallthrough]]
 #  define __UTL_ATTRIBUTE_FALLTHROUGH gnu::fallthrough
 #  define __UTL_ATTRIBUTE_TYPE_CPP_FALLTHROUGH
 #elif UTL_HAS_GNU_ATTRIBUTE(__fallthrough__)
+#  define UTL_FALLTHROUGH __attribute__((__fallthrough__))
 #  define __UTL_ATTRIBUTE_FALLTHROUGH __fallthrough__
 #  define __UTL_ATTRIBUTE_TYPE_GNU_FALLTHROUGH
+#else
+#  define UTL_FALLTHROUGH
 #endif
 
 #if UTL_HAS_CPP_ATTRIBUTE(clang::noescape)
@@ -222,22 +286,33 @@
 #endif
 
 #if UTL_HAS_CPP_ATTRIBUTE(clang::nodebug)
+#  define UTL_NODEBUG [[clang::nodebug]]
 #  define __UTL_ATTRIBUTE_NODEBUG clang::nodebug
 #  define __UTL_ATTRIBUTE_TYPE_CPP_NODEBUG
+#else
+#  define UTL_NODEBUG
 #endif
 
 #if UTL_HAS_CPP_ATTRIBUTE(likely)
+#  define UTL_LIKELY [[likely]]
 #  define __UTL_ATTRIBUTE_LIKELY likely
 #  define __UTL_ATTRIBUTE_TYPE_CPP_LIKELY
 #elif UTL_HAS_CPP_ATTRIBUTE(clang::likely)
+#  define UTL_LIKELY [[clang::likely]]
 #  define __UTL_ATTRIBUTE_LIKELY clang::likely
 #  define __UTL_ATTRIBUTE_TYPE_CPP_LIKELY
+#else
+#  define UTL_LIKELY
 #endif /* UTL_HAS_CPP_ATTRIBUTE(likely) */
 
 #if UTL_HAS_CPP_ATTRIBUTE(unlikely)
+#  define UTL_UNLIKELY [[unlikely]]
 #  define __UTL_ATTRIBUTE_UNLIKELY unlikely
 #  define __UTL_ATTRIBUTE_TYPE_CPP_UNLIKELY
 #elif UTL_HAS_CPP_ATTRIBUTE(clang::unlikely)
+#  define UTL_UNLIKELY [[clang::unlikely]]
 #  define __UTL_ATTRIBUTE_UNLIKELY clang::unlikely
 #  define __UTL_ATTRIBUTE_TYPE_CPP_UNLIKELY
+#else
+#  define UTL_UNLIKELY
 #endif /* UTL_HAS_CPP_ATTRIBUTE(unlikely) */
