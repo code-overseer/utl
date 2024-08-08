5f55fd9ef793d3be988b2571b072751f001df842
Bryan Wong <wongjengyan@gmail.com>
2024-08-08 17:13:14 +0900

    inline

--- src/utl/public/utl/string/utl_char_traits.h
+++ src/utl/public/utl/string/utl_char_traits.h
@@ -30,48 +30,48 @@ struct UTL_PUBLIC_TEMPLATE char_traits {
     using comparison_category = strong_ordering;
 #endif
 
-    UTL_ATTRIBUTE(PURE_API) static constexpr int compare(
+    UTL_ATTRIBUTE(PURE_API) static inline constexpr int compare(
         char_type const* lhs, char_type const* rhs, size_t length) noexcept {
         return libc::strncmp(lhs, rhs, libc::element_count_t(length));
     }
 
-    UTL_ATTRIBUTE(PURE_API) static constexpr size_t length(char_type const* str) noexcept {
+    UTL_ATTRIBUTE(PURE_API) static inline constexpr size_t length(char_type const* str) noexcept {
         return libc::strlen(str);
     }
 
-    UTL_ATTRIBUTE(PURE_API) static constexpr char_type const* find(
+    UTL_ATTRIBUTE(PURE_API) static inline constexpr char_type const* find(
         char_type const* str, size_t length, char_type const ch) noexcept {
         return libc::strnchr(str, ch, libc::element_count_t(length));
     }
 
-    UTL_ATTRIBUTES(HIDE_FROM_ABI, ALWAYS_INLINE) static constexpr char_type* move(
+    UTL_ATTRIBUTES(HIDE_FROM_ABI, ALWAYS_INLINE) static inline constexpr char_type* move(
         char_type* s1, char_type const* s2, size_t length) noexcept {
         return libc::memmove(s1, s2, libc::element_count_t(length));
     }
 
-    UTL_ATTRIBUTES(HIDE_FROM_ABI, ALWAYS_INLINE) static constexpr char_type* copy(
+    UTL_ATTRIBUTES(HIDE_FROM_ABI, ALWAYS_INLINE) static inline constexpr char_type* copy(
         char_type* UTL_RESTRICT dst, char_type const* UTL_RESTRICT src, size_t length) noexcept {
         return libc::memcpy(dst, src, libc::element_count_t(length));
     }
 
-    UTL_ATTRIBUTES(HIDE_FROM_ABI, ALWAYS_INLINE) static constexpr char_type* assign(
+    UTL_ATTRIBUTES(HIDE_FROM_ABI, ALWAYS_INLINE) static inline constexpr char_type* assign(
         char_type* str, size_t length, char_type ch) noexcept {
         return libc::strnset(str, ch, libc::element_count_t(length));
     }
 
-    UTL_ATTRIBUTE(CONST_API) static constexpr int_type not_eof(int_type c) noexcept {
+    UTL_ATTRIBUTE(CONST_API) static inline constexpr int_type not_eof(int_type c) noexcept {
         return eq_int_type(c, eof()) ? ~eof() : c;
     }
-    UTL_ATTRIBUTE(CONST_API) static constexpr char_type to_char_type(int_type c) noexcept {
+    UTL_ATTRIBUTE(CONST_API) static inline constexpr char_type to_char_type(int_type c) noexcept {
         return char_type(c);
     }
-    UTL_ATTRIBUTE(CONST_API) static constexpr int_type to_int_type(char_type c) noexcept {
+    UTL_ATTRIBUTE(CONST_API) static inline constexpr int_type to_int_type(char_type c) noexcept {
         return int_type(as_unsigned(c));
     }
-    UTL_ATTRIBUTE(CONST_API) static constexpr bool eq_int_type(int_type c1, int_type c2) noexcept {
+    UTL_ATTRIBUTE(CONST_API) static inline constexpr bool eq_int_type(int_type c1, int_type c2) noexcept {
         return c1 == c2;
     }
-    UTL_ATTRIBUTE(CONST_API) static constexpr int_type eof() noexcept { return int_type(EOF); }
+    UTL_ATTRIBUTE(CONST_API) static inline constexpr int_type eof() noexcept { return int_type(EOF); }
     UTL_ATTRIBUTE(CONST_API) static inline constexpr bool eq(char_type l, char_type r) noexcept {
         return l == r;
     }
