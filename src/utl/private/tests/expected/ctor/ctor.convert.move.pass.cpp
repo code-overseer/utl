1a6d94ae43ea3bdba77c95924c0cfe29ec11c914
Bryan Wong <wongjengyan@gmail.com>
2024-11-30 12:47:24 +0900

    ctor.convert.move

--- src/utl/private/tests/expected/ctor/ctor.convert.move.pass.cpp
+++ src/utl/private/tests/expected/ctor/ctor.convert.move.pass.cpp
@@ -12,27 +12,27 @@
 // UNSUPPORTED: c++03
 
 //  template<class U, class G>
-//    constexpr explicit(see below) expected(const expected<U, G>&);
+//    constexpr explicit(see below) expected(expected<U, G>&&);
 //
 // Let:
-// - UF be const U&
-// - GF be const G&
+// - UF be const U
+// - GF be const G
 //
 // Constraints:
-// - is_constructible_v<T, UF> is true; and
-// - is_constructible_v<E, GF> is true; and
-// - is_constructible_v<T, expected<U, G>&> is false; and
-// - is_constructible_v<T, expected<U, G>> is false; and
-// - is_constructible_v<T, const expected<U, G>&> is false; and
-// - is_constructible_v<T, const expected<U, G>> is false; and
-// - is_convertible_v<expected<U, G>&, T> is false; and
-// - is_convertible_v<expected<U, G>&&, T> is false; and
-// - is_convertible_v<const expected<U, G>&, T> is false; and
-// - is_convertible_v<const expected<U, G>&&, T> is false; and
-// - is_constructible_v<unexpected<E>, expected<U, G>&> is false; and
-// - is_constructible_v<unexpected<E>, expected<U, G>> is false; and
-// - is_constructible_v<unexpected<E>, const expected<U, G>&> is false; and
-// - is_constructible_v<unexpected<E>, const expected<U, G>> is false.
+// - is_constructible<T, UF>::value is true; and
+// - is_constructible<E, GF>::value is true; and
+// - is_constructible<T, expected<U, G>&>::value is false; and
+// - is_constructible<T, expected<U, G>>::value is false; and
+// - is_constructible<T, const expected<U, G>&>::value is false; and
+// - is_constructible<T, const expected<U, G>>::value is false; and
+// - is_convertible<expected<U, G>&, T>::value is false; and
+// - is_convertible<expected<U, G>&&, T>::value is false; and
+// - is_convertible<const expected<U, G>&, T>::value is false; and
+// - is_convertible<const expected<U, G>&&, T>::value is false; and
+// - is_constructible<unexpected<E>, expected<U, G>&>::value is false; and
+// - is_constructible<unexpected<E>, expected<U, G>>::value is false; and
+// - is_constructible<unexpected<E>, const expected<U, G>&>::value is false; and
+// - is_constructible<unexpected<E>, const expected<U, G>>::value is false.
 //
 // Effects: If rhs.has_value(), direct-non-list-initializes val with utl::forward<UF>(*rhs).
 // Otherwise, direct-non-list-initializes unex with utl::forward<GF>(rhs.error()).
@@ -41,8 +41,8 @@
 //
 // Throws: Any exception thrown by the initialization of val or unex.
 //
-// Remarks: The expression inside explicit is equivalent to !is_convertible_v<UF, T> ||
-// !is_convertible_v<GF, E>.
+// Remarks: The expression inside explicit is equivalent to !is_convertible<UF, T>::value ||
+// !is_convertible<GF, E>::value.
 
 #include "utl/utl_config.h"
 
@@ -61,7 +61,7 @@ namespace expected {
 // Test Constraints:
 template <class T1, class Err1, class T2, class Err2>
 using canCstrFromExpected =
-    utl::is_constructible<utl::expected<T1, Err1>, utl::expected<T2, Err2> const&>;
+    utl::is_constructible<utl::expected<T1, Err1>, utl::expected<T2, Err2>&&>;
 
 struct CtorFromInt {
     CtorFromInt(int);
@@ -71,10 +71,10 @@ static_assert(canCstrFromExpected<CtorFromInt, int, int, int>::value, "");
 
 struct NoCtorFromInt {};
 
-// !is_constructible_v<T, UF>
+// !is_constructible<T, UF>::value
 static_assert(!canCstrFromExpected<NoCtorFromInt, int, int, int>::value, "");
 
-// !is_constructible_v<E, GF>
+// !is_constructible<E, GF>::value
 static_assert(!canCstrFromExpected<int, NoCtorFromInt, int, int>::value, "");
 
 template <class T, bool = utl::is_same<T, int>::value>
@@ -93,20 +93,20 @@ struct CtorFrom<T, true> {
     explicit CtorFrom(U&&) = delete;
 };
 
-// is_constructible_v<T, expected<U, G>&>
+// is_constructible<T, expected<U, G>&>::value
 static_assert(!canCstrFromExpected<CtorFrom<utl::expected<int, int>&>, int, int, int>::value, "");
 
-// is_constructible_v<T, expected<U, G>>
-static_assert(!canCstrFromExpected<CtorFrom<utl::expected<int, int>&&>, int, int, int>::value, "");
-
-// is_constructible_v<T, expected<U, G>&>
+// is_constructible<T, expected<U, G>>::value
 // note that this is true because it is covered by the other overload
 //   template<class U = T> constexpr explicit(see below) expected(U&& v);
 // The fact that it is not ambiguous proves that the overload under testing is removed
-static_assert(
-    canCstrFromExpected<CtorFrom<utl::expected<int, int> const&>, int, int, int>::value, "");
+static_assert(canCstrFromExpected<CtorFrom<utl::expected<int, int>&&>, int, int, int>::value, "");
 
-// is_constructible_v<T, expected<U, G>>
+// is_constructible<T, expected<U, G>&>::value
+static_assert(
+    !canCstrFromExpected<CtorFrom<utl::expected<int, int> const&>, int, int, int>::value, "");
+
+// is_constructible<T, expected<U, G>>::value
 static_assert(
     !canCstrFromExpected<CtorFrom<utl::expected<int, int> const&&>, int, int, int>::value, "");
 
@@ -125,91 +125,85 @@ struct ConvertFrom<T, true> {
     ConvertFrom(U&&) = delete;
 };
 
-// is_convertible_v<expected<U, G>&, T>
+// is_convertible<expected<U, G>&, T>::value
 static_assert(
     !canCstrFromExpected<ConvertFrom<utl::expected<int, int>&>, int, int, int>::value, "");
 
-// is_convertible_v<expected<U, G>&&, T>
-static_assert(
-    !canCstrFromExpected<ConvertFrom<utl::expected<int, int>&&>, int, int, int>::value, "");
-
-// is_convertible_v<const expected<U, G>&, T>
+// is_convertible<expected<U, G>&&, T>::value
 // note that this is true because it is covered by the other overload
 //   template<class U = T> constexpr explicit(see below) expected(U&& v);
 // The fact that it is not ambiguous proves that the overload under testing is removed
 static_assert(
-    canCstrFromExpected<ConvertFrom<utl::expected<int, int> const&>, int, int, int>::value, "");
+    canCstrFromExpected<ConvertFrom<utl::expected<int, int>&&>, int, int, int>::value, "");
 
-// is_convertible_v<const expected<U, G>&&, T>
+// is_convertible<const expected<U, G>&, T>::value
+static_assert(
+    !canCstrFromExpected<ConvertFrom<utl::expected<int, int> const&>, int, int, int>::value, "");
+
+// is_convertible<const expected<U, G>&&, T>::value
 static_assert(
     !canCstrFromExpected<ConvertFrom<utl::expected<int, int> const&&>, int, int, int>::value, "");
 
-// Note for below 4 tests, because their E is constructible from cvref of utl::expected<int, int>,
-// unexpected<E> will be constructible from cvref of utl::expected<int, int>
-// is_constructible_v<unexpected<E>, expected<U, G>&>
+// is_constructible<unexpected<E>, expected<U, G>&>::value
 static_assert(!canCstrFromExpected<int, CtorFrom<utl::expected<int, int>&>, int, int>::value, "");
 
-// is_constructible_v<unexpected<E>, expected<U, G>>
+// is_constructible<unexpected<E>, expected<U, G>>::value
 static_assert(!canCstrFromExpected<int, CtorFrom<utl::expected<int, int>&&>, int, int>::value, "");
 
-// is_constructible_v<unexpected<E>, const expected<U, G>&> is false
+// is_constructible<unexpected<E>, const expected<U, G>&>::value is false
 static_assert(
     !canCstrFromExpected<int, CtorFrom<utl::expected<int, int> const&>, int, int>::value, "");
 
-// is_constructible_v<unexpected<E>, const expected<U, G>>
+// is_constructible<unexpected<E>, const expected<U, G>>::value
 static_assert(
     !canCstrFromExpected<int, CtorFrom<utl::expected<int, int> const&&>, int, int>::value, "");
 
 // test explicit
 static_assert(
-    utl::is_convertible<utl::expected<int, int> const&, utl::expected<short, long>>::value, "");
+    utl::is_convertible<utl::expected<int, int>&&, utl::expected<short, long>>::value, "");
 
-// !is_convertible_v<UF, T>
+// !is_convertible<UF, T>::value
 static_assert(
-    utl::is_constructible<utl::expected<CtorFrom<int>, int>, utl::expected<int, int> const&>::value,
-    "");
+    utl::is_constructible<utl::expected<CtorFrom<int>, int>, utl::expected<int, int>&&>::value, "");
 static_assert(
-    !utl::is_convertible<utl::expected<int, int> const&, utl::expected<CtorFrom<int>, int>>::value,
-    "");
+    !utl::is_convertible<utl::expected<int, int>&&, utl::expected<CtorFrom<int>, int>>::value, "");
 
-// !is_convertible_v<GF, E>.
+// !is_convertible<GF, E>::value.
 static_assert(
-    utl::is_constructible<utl::expected<int, CtorFrom<int>>, utl::expected<int, int> const&>::value,
-    "");
+    utl::is_constructible<utl::expected<int, CtorFrom<int>>, utl::expected<int, int>&&>::value, "");
 static_assert(
-    !utl::is_convertible<utl::expected<int, int> const&, utl::expected<int, CtorFrom<int>>>::value,
-    "");
+    !utl::is_convertible<utl::expected<int, int>&&, utl::expected<int, CtorFrom<int>>>::value, "");
 
 struct Data {
-    int i;
-    constexpr Data(int ii) : i(ii) {}
+    MoveOnly data;
+    constexpr Data(MoveOnly&& m) : data(utl::move(m)) {}
 };
 
 constexpr bool test() {
     // convert the value
     {
-        utl::expected<int, int> const e1(5);
-        utl::expected<Data, int> e2 = e1;
+        utl::expected<MoveOnly, int> e1(5);
+        utl::expected<Data, int> e2 = utl::move(e1);
         assert(e2.has_value());
-        assert(e2.value().i == 5);
+        assert(e2.value().data.get() == 5);
         assert(e1.has_value());
-        assert(e1.value() == 5);
+        assert(e1.value().get() == 0);
     }
 
     // convert the error
     {
-        utl::expected<int, int> const e1(utl::unexpect, 5);
-        utl::expected<int, Data> e2 = e1;
+        utl::expected<int, MoveOnly> e1(utl::unexpect, 5);
+        utl::expected<int, Data> e2 = utl::move(e1);
         assert(!e2.has_value());
-        assert(e2.error().i == 5);
+        assert(e2.error().data.get() == 5);
         assert(!e1.has_value());
-        assert(e1.error() == 5);
+        assert(e1.error().get() == 0);
     }
 
     // convert TailClobberer
     {
-        utl::expected<TailClobbererNonTrivialMove<0>, char> const e1;
-        utl::expected<TailClobberer<0>, char> e2 = e1;
+        utl::expected<TailClobbererNonTrivialMove<0>, char> e1;
+        utl::expected<TailClobberer<0>, char> e2 = utl::move(e1);
         assert(e2.has_value());
         assert(e1.has_value());
     }
