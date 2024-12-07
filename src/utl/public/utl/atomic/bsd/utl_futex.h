49b8f284790c9b18c790177b3f07bc9d96ddfe85
Bryan Wong <wongjengyan@gmail.com>
2024-12-07 17:23:41 +0900

    Update futex to use error_code

--- src/utl/public/utl/atomic/bsd/utl_futex.h
+++ src/utl/public/utl/atomic/bsd/utl_futex.h
@@ -13,6 +13,9 @@
 #endif // UTL_TARGET_BSD
 
 #include "utl/memory/utl_addressof.h"
+#include "utl/system_error/utl_errc.h"
+#include "utl/system_error/utl_error_category.h"
+#include "utl/system_error/utl_error_code.h"
 #include "utl/tempus/utl_duration.h"
 #include "utl/type_traits/utl_is_const.h"
 #include "utl/type_traits/utl_is_trivially_copyable.h"
@@ -25,20 +28,6 @@
 UTL_NAMESPACE_BEGIN
 
 namespace futex {
-
-UTL_CONSTEVAL result result::success() noexcept {
-    return result(0);
-}
-
-constexpr bool result::interrupted() const noexcept {
-    return value_ == EINTR;
-}
-constexpr bool result::timed_out() const noexcept {
-    return value_ == ETIMEDOUT;
-}
-constexpr bool result::failed() const noexcept {
-    return value_ != 0;
-}
 
 #if UTL_CXX20
 
@@ -75,9 +64,9 @@ UTL_INLINE_CXX17 constexpr bool is_waitable_v = __UTL futex::details::waitable<T
 
 template <UTL_CONCEPT_CXX20(waitable_type) T>
 UTL_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) auto wait(T* address, T const& value,
-    __UTL tempus::duration t) noexcept -> UTL_ENABLE_IF_CXX11(result, UTL_TRAIT_is_futex_waitable(T)) {
+    __UTL tempus::duration t) noexcept -> UTL_ENABLE_IF_CXX11(error_code, UTL_TRAIT_is_futex_waitable(T)) {
     if (t == __UTL tempus::duration::zero()) {
-        return result{ETIMEDOUT};
+        return error_code{errc::timed_out};
     }
 
     auto const timeout = static_cast<timespec>(t);
@@ -88,15 +77,15 @@ UTL_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) auto wait(T* address, T const& value,
     auto wait_on = reinterpret_cast<uint32_t*>(address);
 
     if (!::futex(wait_on, FUTEX_WAIT, readable_value, timeout_ptr, nullptr)) {
-        return result::success();
+        return error_code{};
     }
 
     auto const error = errno;
     if (error == EAGAIN) {
-        return result::success();
+        return error_code{};
     }
 
-    return result{error};
+    return error_code{error, system_category()};
 }
 
 template <UTL_CONCEPT_CXX20(waitable_type) T>
